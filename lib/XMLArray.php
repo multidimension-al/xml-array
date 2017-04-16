3732a169ef48060ed9cb106300ed3371aa2794b7
AJ Quick <ajquick@gmail.com>
2017-04-15 23:27:35 -0600

    Bug fixes. 100% code coverage. PHPSniffer Code Beautified.

--- lib/XMLArray.php
+++ lib/XMLArray.php
@@ -1,57 +1,59 @@
 <?php
-/**    __  ___      ____  _     ___                           _                    __
- *    /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
- *   / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ / 
- *  / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /  
- * /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/   
- *                                                                                  
- * CONFIDENTIAL
+/**
+ *      __  ___      ____  _     ___                           _                    __
+ *     /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
+ *    / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ /
+ *   / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /
+ *  /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/
  *
- * © 2017 Multidimension.al - All Rights Reserved
- * 
- * NOTICE:  All information contained herein is, and remains the property of
- * Multidimension.al and its suppliers, if any.  The intellectual and
- * technical concepts contained herein are proprietary to Multidimension.al
- * and its suppliers and may be covered by U.S. and Foreign Patents, patents in
- * process, and are protected by trade secret or copyright law. Dissemination
- * of this information or reproduction of this material is strictly forbidden
- * unless prior written permission is obtained.
+ *  @author Multidimension.al
+ *  @copyright Copyright © 2016-2017 Multidimension.al - All Rights Reserved
+ *  @license Proprietary and Confidential
+ *
+ *  NOTICE:  All information contained herein is, and remains the property of
+ *  Multidimension.al and its suppliers, if any.  The intellectual and
+ *  technical concepts contained herein are proprietary to Multidimension.al
+ *  and its suppliers and may be covered by U.S. and Foreign Patents, patents in
+ *  process, and are protected by trade secret or copyright law. Dissemination
+ *  of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained.
  */
 
 namespace Multidimensional\XmlArray;
 
-class XMLArray {
+class XMLArray
+{
         
     /**
      * @param string $xml
      * @return array
      */
     public function generateArray($string)
-    {        
+    {
         $xml = simplexml_load_string($string);
         $json = json_encode($xml);
-		$array = json_decode($json, TRUE);
-		$array = $this->convertAttributes($array);		
+        $array = json_decode($json, true);
+        $array = $this->convertAttributes($array);
         return $array;
     }
-	
-	/**
-	 * @param array $array
-	 * @return array
-	 */
-	private function convertAttributes($array)
-	{
-		if (is_array($array)) {
-			foreach ($array AS $key => $value) {
-				if(isset($value['@attributes'])) {
-					foreach ($value['@attributes'] AS $key2 => $value2) {
-						$array[$key]['@'.$key2] = $value2;	
-					}
-					unset($array[$key]['@attributes']);	
-				}
-			}
-		}
-		
-		return $array;	
-	}
+    
+    /**
+     * @param array $array
+     * @return array
+     */
+    private function convertAttributes($array)
+    {
+        if (is_array($array)) {
+            foreach ($array as $key => $value) {
+                if (isset($value['@attributes'])) {
+                    foreach ($value['@attributes'] as $key2 => $value2) {
+                        $array[$key]['@'.$key2] = $value2;
+                    }
+                    unset($array[$key]['@attributes']);
+                }
+            }
+        }
+        
+        return $array;
+    }
 }
--- tests/XMLArrayTest.php
+++ tests/XMLArrayTest.php
@@ -1,21 +1,22 @@
 <?php
-/**    __  ___      ____  _     ___                           _                    __
- *    /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
- *   / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ / 
- *  / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /  
- * /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/   
- *                                                                                  
- * CONFIDENTIAL
+/**
+ *      __  ___      ____  _     ___                           _                    __
+ *     /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
+ *    / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ /
+ *   / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /
+ *  /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/
  *
- * © 2017 Multidimension.al - All Rights Reserved
- * 
- * NOTICE:  All information contained herein is, and remains the property of
- * Multidimension.al and its suppliers, if any.  The intellectual and
- * technical concepts contained herein are proprietary to Multidimension.al
- * and its suppliers and may be covered by U.S. and Foreign Patents, patents in
- * process, and are protected by trade secret or copyright law. Dissemination
- * of this information or reproduction of this material is strictly forbidden
- * unless prior written permission is obtained.
+ *  @author Multidimension.al
+ *  @copyright Copyright © 2016-2017 Multidimension.al - All Rights Reserved
+ *  @license Proprietary and Confidential
+ *
+ *  NOTICE:  All information contained herein is, and remains the property of
+ *  Multidimension.al and its suppliers, if any.  The intellectual and
+ *  technical concepts contained herein are proprietary to Multidimension.al
+ *  and its suppliers and may be covered by U.S. and Foreign Patents, patents in
+ *  process, and are protected by trade secret or copyright law. Dissemination
+ *  of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained.
  */
 
 namespace Multidimensional\XmlArray\Test;
@@ -36,31 +37,31 @@ class XMLArrayTest extends TestCase
  
     public function testComplex()
     {
-        $string = '<person><firstname>Test</firstname><lastname>Man</lastname><address><street>123 Fake St</street><city>Springfield</city><state>USA</state></address></person>'; 
+        $string = '<person><firstname>Test</firstname><lastname>Man</lastname><address><street>123 Fake St</street><city>Springfield</city><state>USA</state></address></person>';
         $result = (new XMLArray)->generateArray($string);
         $expected = array ('firstname' => 'Test', 'lastname' => 'Man', 'address' => array ('street' => '123 Fake St', 'city' => 'Springfield', 'state' => 'USA',),);
         $this->assertEquals($result, $expected);
     }
-	
-	public function testAttributes()
-	{
-		$string = '<xml><person ID="1"><firstname>Test</firstname><lastname>Man</lastname><address><street>123 Fake St</street><city>Springfield</city><state>USA</state></address></person></xml>'; 
+    
+    public function testAttributes()
+    {
+        $string = '<xml><person ID="1"><firstname>Test</firstname><lastname>Man</lastname><address><street>123 Fake St</street><city>Springfield</city><state>USA</state></address></person></xml>';
         $result = (new XMLArray)->generateArray($string);
         $expected = array('person' => array ('firstname' => 'Test', 'lastname' => 'Man', 'address' => array ('street' => '123 Fake St', 'city' => 'Springfield', 'state' => 'USA',), '@ID' => '1'));
         $this->assertEquals($result, $expected);
-	}
+    }
     
     public function testFalse()
     {
-        $string = ''; 
+        $string = '';
         $result = (new XMLArray)->generateArray($string);
         $this->assertFalse($result);
     }
     
     public function testNull()
     {
-        $string = null; 
+        $string = null;
         $result = (new XMLArray)->generateArray($string);
-        $this->assertFalse($result);    
+        $this->assertFalse($result);
     }
 }
